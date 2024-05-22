67a29b31e7ddf00297356f1b90c1194c928764f6
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2024-05-22 20:05:28 +0900

    Add files via upload

---
+++ EQDefines.cpp
@@ -0,0 +1,12 @@
+﻿//---------------------------------------------------------------------------
+#include <DateUtils.hpp>
+#include <map>
+#pragma hdrstop
+
+#include "EQDefines.h"
+#include "HelperFunctions.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+TStringList *DAMPER_TYPE_STRING_LIST;
+
