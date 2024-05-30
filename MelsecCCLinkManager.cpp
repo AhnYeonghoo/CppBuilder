c42b48d1e75cdf52230d647feafd68af4e64a404
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2024-05-30 15:32:37 +0900

    Add files via upload

---
+++ MelsecCCLinkManager.cpp
@@ -0,0 +1,68 @@
+﻿//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "MelsecCCLinkManager.h"
+#include "uMelsecCCLinkIoCtrl.h"
+#include "EQDefines.h"
+#include "IODefines.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+MelsecCCLinkManager::MelsecCCLinkManager()
+	: IOControlManager()
+{
+	CriticalSection = new TCriticalSection;
+	IODevices.push_back(nullptr);
+}
+
+MelsecCCLinkManager::~MelsecCCLinkManager()
+{
+	if (CriticalSection)
+	{
+		delete CriticalSection;
+	}
+}
+
+bool __fastcall MelsecCCLinkManager::GetValue(unsigned int addr)
+{
+	bool ret = false;
+	CriticalSection->Acquire();
+	{
+		int moduleNo = addr / MAX_IO_COUNT;
+		int offset = addr;
+		if (IODevices[moduleNo] != nullptr)
+		{
+			ret = IODevices[moduleNo]->GetValue(offset);
+		}
+	}
+    CriticalSection->Release();
+	return ret;
+}
+
+void __fastcall MelsecCCLinkManager::SetOutputValue(unsigned int addr, bool value)
+{
+	CriticalSection->Acquire();
+	{
+		int moduleNo = 1;//CMelsecCCLinkIoCtrl::GetShiftVal(addr);
+		int offset = addr;
+		if (IODevices[moduleNo] != nullptr)
+		{
+			IODevices[moduleNo]->SetOutputValue(offset, value);
+		}
+	}
+	CriticalSection->Release();
+}
+
+bool __fastcall MelsecCCLinkManager::GetIsOutput(unsigned int addr)
+{
+	int moduleNo = addr / MAX_INPUT_COUNT;
+	int offset = addr;
+
+	bool ret = false;
+	if (IODevices[moduleNo] != nullptr)
+	{
+        ret = IODevices[moduleNo]->IsOutput(offset);
+	}
+	return ret;
+}
---
+++ MelsecCCLinkManager.h
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef MelsecCCLinkManagerH
+#define MelsecCCLinkManagerH
+//---------------------------------------------------------------------------
+#include "IOControlManager.h"
+
+#define MAX_IO_COUNT		64
+#define MAX_INPUT_COUNT     32
+#define MAX_OUTPUT_COUNT    32
+
+class MelsecCCLinkManager : public IOControlManager
+{
+protected:
+
+public:
+	MelsecCCLinkManager();
+	~MelsecCCLinkManager();
+
+    bool __fastcall GetIsOutput(unsigned int addr);
+	bool __fastcall GetValue(unsigned int addr);
+	void __fastcall SetOutputValue(unsigned int addr, bool value);
+};
+#endif
