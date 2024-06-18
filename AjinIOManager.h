9a2d8460f1b95356de9f707c58c2774ca113b552
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2024-06-18 17:12:18 +0900

    Add files via upload

---
+++ AjinIOManager.h
@@ -0,0 +1,22 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef AjinIOManagerH
+#define AjinIOManagerH
+//---------------------------------------------------------------------------
+#include "IOControlManager.h"
+
+class AjinIOManager : public IOControlManager
+{
+protected:
+	std::vector<int> AjinIOModuleType;
+
+public:
+	AjinIOManager();
+	~AjinIOManager();
+
+	bool __fastcall GetIsOutput(unsigned int addr);
+	bool __fastcall GetValue(unsigned int addr);
+	void __fastcall SetOutputValue(unsigned int addr, bool value);
+};
+
+#endif
